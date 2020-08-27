d98e631a0e33979d4c4aca02de560efb81e644c1
Mikel Bishop <mdrew8@gmail.com>
2020-08-27 22:37:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,15 +2,6 @@ def line(katz_deli)
   if line = 0 
     puts "The line is currently empty."
     elsif line > 0 
-    puts "You are currently number #{customer}""
+    puts "You are currently number #{customer}"
   end
 end
-
-#first method worked until I added the second method.  Need to reread and fix.
-
-#def line(other_deli)
- # counter = 1 
-  #other_deli.each do |name|
-   # puts "Theh line is currently: #{name}"
-  #end
-#end
