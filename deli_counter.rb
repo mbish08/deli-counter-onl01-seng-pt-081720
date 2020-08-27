40dfb50ab2c677d0d4bdbfdb963a7d9d31406933
Mikel Bishop <mdrew8@gmail.com>
2020-08-27 22:43:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,11 @@
 def line(katz_deli)
   if katz_deli = 0 
     puts "The line is currently empty."
-    elsif other_deli > 0 
-    katz_deli.each_with_index do |customer|
+  end
+  
+  def line(other_deli)
+    if other_deli > 0 
+    other_deli.each_with_index do |customer|
     puts "You are currently number #{customer}"
   end
-end
 end
