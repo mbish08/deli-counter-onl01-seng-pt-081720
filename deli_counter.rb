66322fcccfc8ba37ef7f38d4801ef025b9005aec
Mikel Bishop <mdrew8@gmail.com>
2020-08-27 22:53:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,13 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
+  else
+    position = 1 
+    katz_deli.each do |name|
+      line_positions << "#{position}. #{name}"
+      number += 1 
   end
+  puts "The line is currently: #{line_positions.join(" ")}"
 end
 
 
