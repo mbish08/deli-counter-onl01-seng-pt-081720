db1a675118797274fc195f37cc299747ef64f115
Mikel Bishop <mdrew8@gmail.com>
2020-08-27 22:39:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,7 @@ def line(katz_deli)
   if line = 0 
     puts "The line is currently empty."
     elsif line > 0 
+    katz_deli.each_with_index |customer|
     puts "You are currently number #{customer}"
   end
 end
