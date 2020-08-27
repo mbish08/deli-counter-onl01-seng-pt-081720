c82acd6e3b8f955b744943c68afa6ccce669dc5f
Mikel Bishop <mdrew8@gmail.com>
2020-08-27 22:29:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def line(katz_deli)
+  puts "The line is currently empty."
+end
+
+#first method worked until I added the second method.  Need to reread and fix.
+
+def line(other_deli)
+  counter = 1 
+  other_deli.each do |name|
+    puts "Theh line is currently: #{name}"
+  end
+end
